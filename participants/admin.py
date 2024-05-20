9d832a09b29d61f18934c52a8968a510d93bd2e8
Daniel <daniel.finkle1@gmail.com>
2024-05-20 14:59:48 -0700

    transferring static html to django. there is a readme with basic information. Will update it if not enough

---
+++ participants/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Participant
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    def display_is_organizer(self):
+        if self.isorganizer == True:
+            return "Is organizer"
+        else:
+            return "Not Organizer"
+
+    list_display = [
+        "id",
+        "firstname",
+        "lastname",
+        "workshopcode",
+        "namesuffix",
+        display_is_organizer,
+        "isorganizer",
+    ]
+    search_fields = [
+        "firstname",
+        "lastname",
+    ]
+
+
+admin.site.register(Participant, ParticipantAdmin)
