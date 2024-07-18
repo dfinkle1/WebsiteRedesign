139976edb4752535730a82e771d413af5bc3da3a
Daniel <daniel.finkle1@gmail.com>
2024-07-18 14:25:49 -0700

    Testing Django CMS - updated pages to contain general content

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("workshops.urls")),
-    path("", include("apps.core.urls")),
+    path("", include("apps.core.urls")),  # Your existing app's URLs
 ]
+
+urlpatterns += i18n_patterns(
+    path("", include("cms.urls")),  # Include Django CMS URLs
+)
