00f553a07bf06cb7e863a3a8d13109c7b475e40e
Daniel <daniel.finkle1@gmail.com>
2024-07-23 16:38:06 -0700

    Integrating cms plugins, creating models and templates

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from cms.models.fields import PlaceholderRelationField
+from cms.models.fields import PlaceholderRelationField, PlaceholderField
 from cms.utils.placeholder import get_placeholder_from_slot
 from functools import cached_property
+from cms.models import CMSPlugin
+from filer.fields.image import FilerImageField
 
 
 class MyModel(models.Model):
@@ -16,3 +18,16 @@ class MyModel(models.Model):
         return "templates/testtemplate.html"
 
     # your methods
+
+
+class StaffMember(CMSPlugin):
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    bio = models.TextField()
+    staff_photo = FilerImageField(
+        null=True, blank=True, related_name="profile_picture", on_delete=models.CASCADE
+    )
+    more_info_link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,18 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
+from django.views.i18n import JavaScriptCatalog
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = (
     path("admin/", admin.site.urls),
     path("", include("apps.core.urls")),  # Your existing app's URLs
-]
-
-urlpatterns += i18n_patterns(
     path("", include("cms.urls")),  # Include Django CMS URLs
 )
+if settings.DEBUG:
+    urlpatterns += tuple(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns += tuple(
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
