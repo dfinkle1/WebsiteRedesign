92c121a91445a4ce068c0911fa9adf762b0e2a95
Daniel <daniel.finkle1@gmail.com>
2024-07-24 14:27:47 -0700

    update maybe

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["websiteredesign-production.up.railway.app"]
 
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
