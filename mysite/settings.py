32d76eeb0f3df81169f0af2e658a64d592b9cae7
Daniel <daniel.finkle1@gmail.com>
2024-07-24 16:04:30 -0700

    please work!!

--- mysite/settings.py
+++ mysite/settings.py
@@ -199,8 +199,9 @@ CMS_TEMPLATES = [
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static/")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
