c64d95b18a802726988e9bbc31693b2d9758a393
Daniel <daniel.finkle1@gmail.com>
2024-07-25 10:38:39 -0700

    updating urls.py for static mapping hopefully

--- mysite/settings.py
+++ mysite/settings.py
@@ -218,7 +218,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_DIR = [
 #     BASE_DIR / "static",
 # ]
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
